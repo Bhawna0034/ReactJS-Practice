1394d15b8d5a549a1c06fc8d65b6da19410ad383
Bhawna0034 <bhawnaasharma2002@gmail.com>
2025-02-13 12:50:59 +0530

    Created Movie Card (Interstellar)

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { useState } from 'react'
 import './components/button'; 
-import { AirPods } from './components/AirPodsCard';
+import {MovieCard} from './components/MovieCard'
 
 
 
-import './App.css'
+
+// import './App.css'
 // import MyButton from './components/button';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <AirPods />
+   <MovieCard/> 
   )
 }
 
