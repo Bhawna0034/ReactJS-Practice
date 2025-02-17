763fa384ab17e873a56db1f55d3c9b98f6a4dbf5
Bhawna0034 <bhawnaasharma2002@gmail.com>
2025-02-17 11:44:57 +0530

    Passing JSX as Props

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,13 +1,40 @@
-import profileData from '../api/profileData.json'
-import ProfileCard from './ProfileCard'
+// import profileData from '../api/profileData.json'
+// import ProfileCard from './ProfileCard'
+
+// export default function Profile(){
+//     return(
+//         <ul>
+//             {profileData.map((currentElement) => {
+//                 return <ProfileCard key={currentElement.id} data = {currentElement}/>
+//             })}
+//         </ul>
+//     )
+
+// }
+
+function ProfileCard({name, age, designation, message}) {
+  return (
+    <div>
+      <h1>Name: {name}</h1>
+      <p>Age: {age}</p>
+      <p>Designation: {designation}</p>
+      <p>{message}</p>
+    </div>
+  );
+}
 
 export default function Profile(){
     return(
-        <ul>
-            {profileData.map((currentElement) => {
-                return <ProfileCard key={currentElement.id} data = {currentElement}/>
-            })}
-        </ul>
+        <div>
+            <ProfileCard name = "Bhawna"
+                         age = {22}
+                         designation = "Frontend Developer"
+                         message = {
+                            <div>
+                                <strong>Welcome to React</strong>
+                            </div>
+                         } />
+           
+        </div>
     )
-
 }
--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,11 +1,12 @@
-export default function ProfileCard({data}){
-    const {name, age, designation, message} = data;
-    return(
-        <li>
-            <h1 className = "text-4xl font-bold">Name: {name} </h1>
-            <p className="text-xl">Age: {age}</p>
-            <p className="text-lg">Designation: {designation}</p>
-            <p className="text-lg">{message}</p>
-        </li>
-    )
-}
+// export default function ProfileCard({data}){
+//     const {name, age, designation, message} = data;
+//     return(
+//         <li>
+//             <h1 className = "text-4xl font-bold">Name: {name} </h1>
+//             <p className="text-xl">Age: {age}</p>
+//             <p className="text-lg">Designation: {designation}</p>
+//             <p className="text-lg">{message}</p>
+//         </li>
+//     )
+// }
+
