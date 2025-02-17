e66e6ccb90f32837559841ff8956d1215b656ee6
Bhawna0034 <bhawnaasharma2002@gmail.com>
2025-02-17 11:53:16 +0530

    Passing JSX as Children

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -12,29 +12,23 @@
 
 // }
 
-function ProfileCard({name, age, designation, message}) {
+function ProfileCard({ name, age, designation, children }) {
   return (
     <div>
       <h1>Name: {name}</h1>
       <p>Age: {age}</p>
       <p>Designation: {designation}</p>
-      <p>{message}</p>
+      <p>{children}</p>
     </div>
   );
 }
 
-export default function Profile(){
-    return(
-        <div>
-            <ProfileCard name = "Bhawna"
-                         age = {22}
-                         designation = "Frontend Developer"
-                         message = {
-                            <div>
-                                <strong>Welcome to React</strong>
-                            </div>
-                         } />
-           
-        </div>
-    )
+export default function Profile() {
+  return (
+    <div>
+      <ProfileCard name="Bhawna" age={22} designation="Frontend Developer">
+        <em>Welcome to React👋</em>
+      </ProfileCard>
+    </div>
+  );
 }
