c383134a418c0bced9f322ee01fd0077270d2d5e
Bhawna0034 <bhawnaasharma2002@gmail.com>
2025-02-20 12:08:39 +0530

    Event Stop Propagation

--- src/components/EventPropagation.jsx
+++ src/components/EventPropagation.jsx
@@ -7,16 +7,20 @@ export const EventPropagation = () => {
         console.log("Parent clicked!");
     }
 
-    function handleChildClick(){
+    function handleChildClick(event){
+        console.log(event);
+        event.stopPropagation();
         console.log("Child clicked!");
+    
+        
     }
     return(
        <>
        {/* Bubbling Event */}
        <section className="flex items-center justify-center">
-          <div className="border-2 border-black bg-sky-400 px-24 py-10" onClick={handleGrandParentClick}>
-            <div className="border-2 border-black bg-green-500 px-12 py-6 " onClick={handleParentClick}>
-                <button className="border-2 border-black bg-yellow-300 px-4 py-2" onClick={handleChildClick}>Click Me</button>
+          <div className="border-4 border-black bg-sky-400 px-24 py-10" onClick={handleGrandParentClick}>
+            <div className="border-4 border-black bg-green-500 px-12 py-6 " onClick={handleParentClick}>
+                <button className="border-4 border-black bg-yellow-300 px-4 py-2" onClick={handleChildClick}>Click Me</button>
             </div>
           </div>
        </section>
