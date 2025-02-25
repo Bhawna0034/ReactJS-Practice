3519b070bc83cf5fb16c2a8a36a7bd1f4cd6660f
Bhawna0034 <bhawnaasharma2002@gmail.com>
2025-02-25 15:56:23 +0530

    Displayed user's list through useState()

--- src/components/hooks/DerivedState.jsx
+++ src/components/hooks/DerivedState.jsx
@@ -1,31 +1,39 @@
+import { useState } from "react";
 
-
-const users = [
-    {name: "Arjun Sharma", age: 27},
-    {name: "Bhawna Sharma", age: 22},
-    {name: "Deepak Sharma", age: 30},
-    {name: "Hemant Sharma", age: 24},
-    {name: "Sanya Sharma", age: 26},
-    {name: "Sarthak Sharma", age: 29},
-    {name: "Upasana Medhi", age: 29}
-];
+// const users = [
+//   { name: "Arjun Sharma", age: 27 },
+//   { name: "Bhawna Sharma", age: 22 },
+//   { name: "Deepak Sharma", age: 30 },
+//   { name: "Hemant Sharma", age: 24 },
+//   { name: "Sanya Sharma", age: 26 },
+//   { name: "Sarthak Sharma", age: 29 },
+//   { name: "Upasana Medhi", age: 29 },
+// ];
 
 export const DerivedState = () => {
-   
-    return(
-        <div>
-            <h1>User's List </h1>
-            <ul >
-                {
-                    users.map((currentElement, index) => {
-                        return <>
-                        <li key={index}>
-                            {currentElement.name} - {currentElement.age} years old
-                        </li>
-                        </>
-                    })
-                }
-            </ul>
-        </div>
-    );
-}
+  const [users, setUser] = useState([
+    { name: "Arjun Sharma", age: 27 },
+    { name: "Bhawna Sharma", age: 22 },
+    { name: "Deepak Sharma", age: 30 },
+    { name: "Hemant Sharma", age: 24 },
+    { name: "Sanya Sharma", age: 26 },
+    { name: "Sarthak Sharma", age: 29 },
+    { name: "Upasana Medhi", age: 29 },
+  ]);
+  return (
+    <div>
+      <h1>User's List </h1>
+      <ul>
+        {users.map((currentElement, index) => {
+          return (
+            <>
+              <li key={index}>
+                {currentElement.name} - {currentElement.age} years old
+              </li>
+            </>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
