7fab476e40601d6c6b9235ae1f4a9bcd3925a9bb
Bhawna0034 <bhawnaasharma2002@gmail.com>
2025-03-15 21:59:22 +0530

    Handling Loading and Error States

--- src/components/hooks/useEffect/FetchPikachu.jsx
+++ src/components/hooks/useEffect/FetchPikachu.jsx
@@ -2,20 +2,27 @@ import React, { useEffect, useState } from "react";
 
 const FetchPikachu = () => {
   const [apiData, setApiData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   const API_URL = "https://pokeapi.co/api/v2/pokemon/pikachu";
-  function fetchData() {
-    fetch(API_URL)
-      .then((response) => response.json())
-      .then((data) => setApiData(data))
-      .catch((error) => console.error("Fetching error: ", error));
-  }
+  
 
   useEffect(() => {
-    fetchData();
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        setLoading(false)
+        setApiData(data)})
+     
+      .catch((error) => {
+        setLoading(false);
+        setError(error)
+        console.error("Fetching error: ", error)});
   }, []);
 
-  if (apiData === null) return <h2>Loading...</h2>;
+  if(loading) return <h2 className="text-5xl font-bold">Loading...</h2>
+  if(error) return <h2 className="text-xs text-red-600">{error.message}</h2>
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h2 className="text-4xl font-bold">Pokemon Card</h2>
@@ -26,12 +33,12 @@ const FetchPikachu = () => {
         <ul>
           <li>
             <figure>
-                <img src = {"pokemon.sprites.other.dream_world.front_default"}
+                <img src = {"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"}
                      alt="pikachu"
                      className="w-[100px] object-contain"
                      />
             </figure>
-            <h2 className="text-2xl font-bold">{apiData.name}</h2>
+            <h2 className="text-2xl font-bold">{apiData ? apiData.name : "Unknown"}</h2>
           </li>
         </ul>
       </div>
