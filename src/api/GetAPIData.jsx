1825b2fb8e1e2e49dd0d76249ba4b312d164de4d
Bhawna0034 <bhawnaasharma2002@gmail.com>
2025-04-02 17:14:59 +0530

    Secure Your React App: Using ENV Variables to Protect Private Data

--- src/api/GetAPIData.jsx
+++ src/api/GetAPIData.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
 const getMovieData = async() => {
+  const API_KEY = import.meta.env.VITE_API_KEY;
+
     try{
-        const response = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=e6dd65d1&s=harry+potter&page=1");
-        const data = response.json();
+        const response = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}&s=harry+potter&page=1`);
+        const data = await response.json();
         return data;
+      
+
 
     }catch(error){
         console.log(error);
